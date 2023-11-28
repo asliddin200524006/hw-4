8ccbcfed9be05b2f2990efca47e7168082e6293e
asliddin200524006 <149239813+asliddin200524006@users.noreply.github.com>
2023-11-28 18:31:29 +0600

    Add files via upload

---
+++ homew-4/main.js
@@ -0,0 +1,35 @@
+let users = [
+    {id: 1, name: "Вася"},
+    {id: 2, name: "Петя"},
+    {id: 3, name: "Маша"}
+]
+
+// let user = users.find(item => item.id === 2)
+
+// console.log(user.name);
+
+
+let user = users.filter(item => item.id > 1)
+
+if(id => 1){
+    console.log(user);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
